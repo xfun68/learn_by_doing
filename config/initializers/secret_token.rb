d6e58a336d28d718ce3d8f2f7a09757337aad9b5
Xiaofeng Wang <xfun68@gmail.com>
2013-07-19 16:37:59 +0800

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxifyYourSite::Application.config.secret_token = '10c49f340811c439c2ae1fdea972c5e6d0a2fc2f05f70263f4e757d762027b02cda77aabfae064b789ee8a2cb19edfe3ae7149a3f1095151686eb17e10bac6dd'

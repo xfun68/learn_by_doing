19d5659ddc0064ddf02df10b8db30edea9cdf3f3
Xiaofeng Wang <xfun68@gmail.com>
2014-04-20 17:36:03 +0800

    create requestHanlders and handle

--- node.js/first-web-app/server.js
+++ node.js/first-web-app/server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var url = require('url');
 
-function start(route) {
+function start(route, handle) {
   var onRequest = function(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received');
 
-    route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, {'Content-Tpye': 'text/plain'});
     response.write('Hello World');

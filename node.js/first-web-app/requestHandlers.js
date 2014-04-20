670d18911273abb2b6546afaef62d5c25ceecc7a
Xiaofeng Wang <xfun68@gmail.com>
2014-04-20 19:13:10 +0800

    use formidable to process uploaded image

--- node.js/first-web-app/requestHandlers.js
+++ node.js/first-web-app/requestHandlers.js
@@ -1,8 +1,9 @@
 var exec = require('child_process').exec;
 var querystring = require('querystring');
 var fs = require('fs');
+var formidable = require('formidable');
 
-function start(response, postData) {
+function start(response, request) {
   console.log("Request handler 'start' was called.");
 
   var body = '<html>'+
@@ -23,15 +24,24 @@ function start(response, postData) {
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called.");
 
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('You have sent: ' + querystring.parse(postData).text);
-  response.end();
+  var form = formidable.IncomingForm();
+  console.log('about to parse');
+
+  form.parse(request, function (error, fields, files) {
+    console.log('parsing done');
+
+    fs.renameSync(files.upload.path, '/tmp/test.png');
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('received image:<br />');
+    response.write('<img src="/show" />');
+    response.end();
+  });
 }
 
-function show(response, postData) {
+function show(response, request) {
   console.log("Request handler 'show' was called.");
 
   fs.readFile('/tmp/test.png', 'binary', function (error, file) {

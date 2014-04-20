f7f3b789360ca6393b93e65a60286dc7494a4d87
Xiaofeng Wang <xfun68@gmail.com>
2014-04-20 16:59:57 +0800

    make server.js as a module

--- node.js/first-web-app/server.js
+++ node.js/first-web-app/server.js
@@ -1,13 +1,17 @@
 var http = require('http');
 
-var onRequest = function(request, response) {
-  console.log('Request received');
-  response.writeHead(200, {'Content-Tpye': 'text/plain'});
-  response.write('Hello World');
-  response.end();
+function start() {
+  var onRequest = function(request, response) {
+    console.log('Request received');
+    response.writeHead(200, {'Content-Tpye': 'text/plain'});
+    response.write('Hello World');
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+
+  console.log('Server has started.');
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('Server has started.');
+exports.start = start;
 

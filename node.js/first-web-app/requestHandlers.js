9137e6aef87600c5af3be8faeaf567e2dc8af5a3
Xiaofeng Wang <xfun68@gmail.com>
2014-04-20 19:01:04 +0800

    change form template to support formidable

--- node.js/first-web-app/requestHandlers.js
+++ node.js/first-web-app/requestHandlers.js
@@ -11,9 +11,9 @@ function start(response, postData) {
     'charset=UTF-8" />'+
     '</head>'+
     '<body>'+
-    '<form action="/upload" method="post">'+
-    '<textarea name="text" rows="20" cols="60"></textarea>'+
-    '<input type="submit" value="Submit text" />'+
+    '<form action="/upload" enctype="multipart/form-data" method="post">'+
+    '<input type="file" name="upload" />' +
+    '<input type="submit" value="Upload file" />'+
     '</form>'+
     '</body>'+
     '</html>';

e574f82a5947338b8f43d752a60d4fd993a64a04
Xiaofeng Wang <xfun68@gmail.com>
2014-04-20 18:57:00 +0800

    new request handler to show image

--- node.js/first-web-app/requestHandlers.js
+++ node.js/first-web-app/requestHandlers.js
@@ -1,5 +1,6 @@
 var exec = require('child_process').exec;
 var querystring = require('querystring');
+var fs = require('fs');
 
 function start(response, postData) {
   console.log("Request handler 'start' was called.");
@@ -30,6 +31,23 @@ function upload(response, postData) {
   response.end();
 }
 
+function show(response, postData) {
+  console.log("Request handler 'show' was called.");
+
+  fs.readFile('/tmp/test.png', 'binary', function (error, file) {
+    if (error) {
+      response.writeHead(500, {'Content-Type': 'text/plain'});
+      response.write(error + "\n");
+      response.end();
+    } else {
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.write(file, 'binary');
+      response.end();
+    }
+  });
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
 

5c9dfbbdd0a17213f15b38c3c0bcbfa51f24c935
Xiaofeng Wang <xfun68@gmail.com>
2015-03-22 16:31:41 +0800

    Display sized lcd numbers

--- cpp/lcd_numbers/lcd_numbers.cpp
+++ cpp/lcd_numbers/lcd_numbers.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
 
 typedef std::vector<std::string> lcd_number_t;
 
 class LCDNumber {
   public:
-    LCDNumber(const std::string& number) {
+    LCDNumber(const std::string& number, size_t size = 1) : size(size) {
       if (number == "0") {
         lines.push_back(" - "); lines.push_back("| |"); lines.push_back("   "); lines.push_back("| |"); lines.push_back(" - ");
       } else if (number == "1") {
@@ -30,6 +31,8 @@ class LCDNumber {
       } else {
         lines.push_back(" - "); lines.push_back("|  "); lines.push_back(" - "); lines.push_back("|  "); lines.push_back(" - ");
       }
+
+      scale();
     }
 
     size_t lines_count() const {
@@ -41,6 +44,44 @@ class LCDNumber {
     }
 
   private:
+    void scale() {
+      scale_v();
+      scale_h();
+    }
+
+    void scale_v() {
+      std::vector<std::string> scaled_lines;
+
+      for (size_t i = 0; i < lines.size(); ++i) {
+        size_t repeats = ((i%2) == 1) ? size : 1;
+
+        for (size_t count = 0; count < repeats; ++count) {
+          scaled_lines.push_back(lines[i]);
+        }
+      }
+
+      lines = scaled_lines;
+    }
+
+    void scale_h() {
+      for (size_t i = 0; i < lines.size(); ++i) {
+        std::string& line = lines[i];
+        std::string scaled_line;
+
+        for (size_t j = 0; j < line.size(); ++j) {
+          size_t repeats = ((j%2) == 1) ? size : 1;
+
+          for (size_t count = 0; count < repeats; ++count) {
+            scaled_line += line[j];
+          }
+        }
+
+        lines[i] = scaled_line;
+      }
+    }
+
+  private:
+    size_t size;
     std::vector<std::string> lines;
 };
 
@@ -75,18 +116,19 @@ class Display {
 };
 
 void print(const LCDNumber& lcd_number);
-void parse_parameters(int argc, char**& argv);
+void parse_parameters(int argc, char**& argv, size_t& size, std::vector<std::string>& numbers);
 
 int main(int argc, char* argv[]) {
   try {
-    parse_parameters(argc, argv);
+    size_t size = 0;
+    std::vector<std::string> numbers;
 
-    std::string numbers(argv[1]);
+    parse_parameters(argc, argv, size, numbers);
 
     Display display;
 
     for (size_t i = 0; i < numbers.size(); ++i) {
-      LCDNumber lcd_number(numbers.substr(i, 1));
+      LCDNumber lcd_number(numbers[i], size);
       display.add(lcd_number);
     }
 
@@ -94,8 +136,9 @@ int main(int argc, char* argv[]) {
   } catch (std::string& error)
   {
     std::cout << error << std::endl;
-  } catch (...) {
+  } catch (std::exception& e) {
     std::cout << "Error: Something wrong just happened!" << std::endl;
+    std::cout << "\t" << e.what() << std::endl;
   }
 
   return 0;
@@ -107,11 +150,28 @@ void print(const LCDNumber& lcd_number) {
   }
 }
 
-void parse_parameters(int argc, char**& argv)
+void parse_parameters(int argc, char**& argv, size_t& size, std::vector<std::string>& numbers)
 {
-  if (argc != 2) {
-    std::cout << "Error: bad parameters!" << std::endl;
-    throw("Wrong arguments!");
+  const char* numbers_parameter = NULL;
+
+  if (argc == 2) {
+    size = 1;
+    numbers_parameter = argv[1];
+  } else if (argc == 4) {
+    const char* argument = argv[1];
+    if (std::string("-s") != argument) {
+      throw(std::runtime_error(std::string("Unknown argument: ") + argument));
+    }
+    size = (size_t)atoi(argv[2]);
+    numbers_parameter = argv[3];
+  } else {
+    throw(std::runtime_error("Wrong arguments!"));
+  }
+
+  std::string numbers_string(numbers_parameter);
+
+  for (size_t i = 0; i < numbers_string.size(); ++i) {
+    numbers.push_back(numbers_string.substr(i, 1));
   }
 }
 

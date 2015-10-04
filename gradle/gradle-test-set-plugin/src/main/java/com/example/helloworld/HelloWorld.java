94bf0c9e008e4a1c22c11b97a0f3de4744e39548
Xiaofeng Wang <xfun68@gmail.com>
2015-10-04 15:17:40 +0800

    copy from first-java-jar-project

---
+++ gradle/gradle-test-set-plugin/src/main/java/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.helloworld;
+
+import org.apache.log4j.Logger;
+
+public class HelloWorld {
+    private static final Logger LOGGER = Logger.getLogger(String.valueOf(HelloWorld.class));
+
+    public static void main(String[] args) {
+        MessageService messageService = new MessageService();
+        String message = messageService.getMessage();
+        LOGGER.info("Received message: " + message);
+    }
+}
---
+++ gradle/gradle-test-set-plugin/src/main/java/com/example/helloworld/MessageService.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+
+public class MessageService {
+    public String getMessage() {
+        return "Hello World!";
+    }
+}

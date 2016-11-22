8132ad9ddb28a1d059b79a747a0b5c725304e67e
Xiaofeng Wang <xfun68@gmail.com>
2016-11-22 19:35:34 +0800

    first commit for feign-client-demo

---
+++ spring/feign-client-demo/src/main/java/com/example/FeignClientDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientDemoApplication.class, args);
+	}
+}
